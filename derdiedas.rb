106c4721cc0a40ae48b059c9242b6f24daaf3a38
Hana Harencarova <h.harencarova@gmail.com>
2020-03-24 23:07:30 +0100

    First version

---
+++ derdiedas.rb
@@ -0,0 +1,45 @@
+# the app should present the user with a german word, wait for his guess on article
+# and say if he's right
+# it should have sets
+# rating for each word, how good remembered it was and when it was practiced last time
+# options - a batch or whole dictionary
+
+all_words = {
+  Sonne: 'die',
+  Wasser: 'das',
+  Kind: 'das',
+  Stern: 'der',
+  Ofen: 'der',
+}
+
+puts "Welcome! Let's test your knowledge of German articles"
+
+puts "If you want to quit write 'quit'."
+puts "Instructions:\n1-Play\n2-Add a new word\n3-Write 'quit' if you want to quit.\n"
+print ">"
+
+option = gets.chomp
+puts option
+
+case option
+when '1'
+  puts "Let's play."
+
+  guess = nil
+  loop do
+    word = all_words.keys.sample
+    puts word
+    guess = gets.chomp.downcase
+    if guess == all_words[word]
+      puts "Good answer.\n---"
+    elsif guess == 'quit'
+      break
+    else
+      puts "Sorry, that's not right.\n---"
+    end
+  end
+when '2'
+  puts 'Write your words'
+when '3'
+
+end
