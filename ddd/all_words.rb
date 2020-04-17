25531e597094fd391f6f46c82d91a31f7cc3bbbd
Hana Harencarova <h.harencarova@gmail.com>
2020-04-17 22:27:30 +0200

    Implement adding a word

---
+++ ddd/all_words.rb
@@ -0,0 +1,11 @@
+require_relative 'word'
+
+wolke = Word.new('die', 'Wolke')
+sonne = Word.new('die', 'Sonne')
+katze = Word.new('die', 'Katze')
+kind = Word.new('das', 'Kind')
+wasser = Word.new('das', 'Wasser')
+stern = Word.new('der', 'Stern')
+ofen = Word.new('der', 'Ofen')
+
+@all_words = Word.all
---
+++ ddd/derdiedas.rb
@@ -0,0 +1,48 @@
+# TO DO
+# implement sets of words
+# rating for each word, how good remembered it was and when it was practiced last time
+# options - a batch or whole dictionary
+
+require_relative 'word'
+require_relative 'all_words'
+
+WORD_PATTERN = /(?<article>d(ie|er|as))\s(?<word>\w*)/
+
+puts "Welcome! Let's test your knowledge of German articles\n"
+puts "Instructions:\n1-Play\n2-Add new words\n3-Write 'quit' if you want to quit.\n"
+print ">"
+
+option = gets.chomp
+
+case option
+when '1'
+  puts "Let's play."
+
+  guess = nil
+  loop do
+    word = @all_words[rand(0..@all_words.count-1)]
+    puts word.word
+    guess = gets.chomp.downcase
+    if guess == word.article
+      puts "Great!\n---"
+    elsif guess == 'quit'
+      break
+    else
+      puts "Sorry, that's not right.\n---"
+    end
+  end
+when '2'
+  puts 'Write your words:'
+
+  loop do
+    new_word = gets.chomp
+    if match = new_word.match(WORD_PATTERN)
+      new_word_article, new_word_base = match.captures
+      Word.new(new_word_article, new_word_base) #this works just the word is lost after leaving the app
+    elsif new_word == 'quit'
+      break
+    else
+      puts "Sorry, I can't understand the word. Please write an article and the noun."
+    end
+  end
+end
